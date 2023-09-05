398e1b1bea153dcf84a684f80d7fa0b774944a20
Alla Yevlampiieva <modan1679@gmail.com>
2023-09-05 05:00:00 +0300

    Update Searchbar (hooks)

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { ImSearch } from 'react-icons/im';
 import {
@@ -8,45 +8,42 @@ import {
   SearchFormInput,
 } from './Searchbar.styled';
 
-export class Searchbar extends Component {
-  state = {
-    query: '',
+export const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const SearchQuery = event => {
+    setQuery(event.currentTarget.value.toLowerCase());
   };
 
-  handleSearchQuery = event => {
-    this.setState({ query: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const SubmitSearchQuery = event => {
     event.preventDefault();
 
-    if (this.state.query.trim() === '') {
+    if (query.trim() === '') {
       toast.error('Please input some value');
       return;
     }
 
-    this.props.onSubmit(this.state.query);
-    this.setState({ query: '' });
+    onSubmit(query);
+    setQuery('');
   };
-  render() {
-    return (
-      <SearchbarHeader>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchFormBtn type="submit">
-            <ImSearch style={{ width: 20, height: 20 }} />
-          </SearchFormBtn>
 
-          <SearchFormInput
-            type="text"
-            name="query"
-            value={this.state.query}
-            onChange={this.handleSearchQuery}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-      </SearchbarHeader>
-    );
-  }
-}
+  return (
+    <SearchbarHeader>
+      <SearchForm onSubmit={SubmitSearchQuery}>
+        <SearchFormBtn type="submit">
+          <ImSearch style={{ width: 20, height: 20 }} />
+        </SearchFormBtn>
+
+        <SearchFormInput
+          type="text"
+          name="query"
+          value={query}
+          onChange={SearchQuery}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </SearchbarHeader>
+  );
+};
