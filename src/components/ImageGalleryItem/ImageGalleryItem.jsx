54f4901743d9d55a6a799d41ec217be4ec9f82d5
Alla Yevlampiieva <modan1679@gmail.com>
2023-09-05 04:45:00 +0300

    Update ImageGalleryItem (hooks)

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,31 +1,53 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ModalImage } from 'components/Modal/Modal';
 import { Image } from './ImageGalleryItem.styled';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({
+  image: { webformatURL, largeImageURL, tags },
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(isModalOpen => !isModalOpen);
   };
 
-  toggleModal = () => {
-    this.setState(prevState => ({
-      isModalOpen: !prevState.isModalOpen,
-    }));
-  };
+  return (
+    <>
+      <Image src={webformatURL} alt={tags} onClick={toggleModal} />
+      <ModalImage
+        isOpen={isModalOpen}
+        largeImageURL={largeImageURL}
+        tags={tags}
+        onClose={toggleModal}
+      />
+    </>
+  );
+};
 
-  render() {
-    const { isModalOpen } = this.state;
-    const { webformatURL, largeImageURL, tags } = this.props.image;
-    return (
-      <>
-        <Image src={webformatURL} alt={tags} onClick={this.toggleModal} />
-        <ModalImage
-          isOpen={isModalOpen}
-          largeImageURL={largeImageURL}
-          tags={tags}
-          onClose={this.toggleModal}
-        />
-      </>
-    );
-  }
-}
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     isModalOpen: false,
+//   };
+
+//   toggleModal = () => {
+//     this.setState(prevState => ({
+//       isModalOpen: !prevState.isModalOpen,
+//     }));
+//   };
+
+//   render() {
+//     const { isModalOpen } = this.state;
+//     const { webformatURL, largeImageURL, tags } = this.props.image;
+//     return (
+//       <>
+//         <Image src={webformatURL} alt={tags} onClick={this.toggleModal} />
+//         <ModalImage
+//           isOpen={isModalOpen}
+//           largeImageURL={largeImageURL}
+//           tags={tags}
+//           onClose={this.toggleModal}
+//         />
+//       </>
+//     );
+//   }
+// }
